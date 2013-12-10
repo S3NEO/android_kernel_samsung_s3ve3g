de1c291a7795afa7d97d0936ea680f6dc9e8ebac
Vivek Panwar <vpanwa@codeaurora.org>
2013-12-09 23:22:07 -0800

    Revert "msm: mdss: stop display commit thread only if it is running"
    This reverts commit 8d3014ccb917ecf0274d3823a17ea8962daacdbf
    
    Change-Id: I42c19f4f1d5a5c1d55297a97f85f88410a725b00
    Signed-off-by: Panwar Vivek <vpanwa@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1546,7 +1546,6 @@ static int mdss_fb_open(struct fb_info *info, int user)
 			pr_err("unable to start display thread %d\n",
 				mfd->index);
 			result = PTR_ERR(mfd->disp_thread);
-			mfd->disp_thread = NULL;
 			goto thread_error;
 		}
 
@@ -1566,7 +1565,6 @@ static int mdss_fb_open(struct fb_info *info, int user)
 
 blank_error:
 	kthread_stop(mfd->disp_thread);
-	mfd->disp_thread = NULL;
 
 thread_error:
 	if (pinfo && !pinfo->ref_cnt) {
@@ -1607,6 +1605,7 @@ static int mdss_fb_release_all(struct fb_info *info, bool release_all)
 	list_for_each_entry_safe(pinfo, temp_pinfo, &mfd->proc_list, list) {
 		if (!release_all && (pinfo->pid != pid))
 			continue;
+
 		unknown_pid = false;
 
 		pr_debug("found process %s pid=%d mfd->ref=%d pinfo->ref=%d\n",
@@ -1623,10 +1622,8 @@ static int mdss_fb_release_all(struct fb_info *info, bool release_all)
 			pm_runtime_put(info->dev);
 		} while (release_all && pinfo->ref_cnt);
 
-		if (release_all && mfd->disp_thread) {
+		if (release_all)
 			kthread_stop(mfd->disp_thread);
-			mfd->disp_thread = NULL;
-		}
 
 		if (pinfo->ref_cnt == 0) {
 			list_del(&pinfo->list);
@@ -1667,10 +1664,7 @@ static int mdss_fb_release_all(struct fb_info *info, bool release_all)
 #if defined(CONFIG_WHITE_PANEL)
 		mdss_fb_set_backlight(mfd, 0);
 #endif
-		if (mfd->disp_thread) {
-			kthread_stop(mfd->disp_thread);
-			mfd->disp_thread = NULL;
-		}
+		kthread_stop(mfd->disp_thread);
 
 		ret = mdss_fb_blank_sub(FB_BLANK_POWERDOWN, info,
 			mfd->op_enable);
