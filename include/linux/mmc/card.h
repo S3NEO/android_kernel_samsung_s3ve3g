0ec78ec6702dd17413a57f24a737dc35252ef68d
Maya Erez <merez@codeaurora.org>
2014-02-03 23:24:11 +0200

    mmc: card: quirk: disable cache for Hynix card
    This change prevents enabling cache during card init for Hynix cards.
    This change is workaround the problem of Hynix cards behavior for
    adb reboot flow, when as a result of power cycle with cache enabled,
    ext4 journal get aborted, because of meta data corruption.
    
    Signed-off-by: Konstantin Dorfman <kdorfman@codeaurora.org>
    
    Conflicts:
    
    	drivers/mmc/card/block.c
    	include/linux/mmc/card.h
    
    Change-Id: I721689da65e803965f1c7dd148754c4f19a64857
    Signed-off-by: Maya Erez <merez@codeaurora.org>

--- include/linux/mmc/card.h
+++ include/linux/mmc/card.h
@@ -371,6 +371,7 @@ struct mmc_card {
 /* Skip data-timeout advertised by card */
 #define MMC_QUIRK_BROKEN_DATA_TIMEOUT	(1<<13)
 
+#define MMC_QUIRK_CACHE_DISABLE (1 << 14)       /* prevent cache enable */
 	unsigned int		erase_size;	/* erase size in sectors */
  	unsigned int		erase_shift;	/* if erase unit is power 2 */
  	unsigned int		pref_erase;	/* in sectors */
