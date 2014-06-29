6e948b39519664ad39d8e70527d94940c2ec9177
Pranav Vashi <neobuddy89@gmail.com>
2014-06-29 16:34:29 +0530

    sched: Add controls for sched features
    * Currently we provide users attributes to enable/
      disable GENTLE_FAIR_SLEEPERS and ARCH_POWER
    * This patch uses already implemented GFS structure
      to add ARCH_POWER attribute.
    * Attributes can be found under /sys/kernel/sched/*
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	kernel/ksysfs.c
    
    Change-Id: I23773bba6c80508c1f2d1f777fcb4dc6193da837

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3783,7 +3783,7 @@ static void update_cpu_power(struct sched_domain *sd, int cpu)
 	struct sched_group *sdg = sd->groups;
 
 	if ((sd->flags & SD_SHARE_CPUPOWER) && weight > 1) {
-		if (sched_feat(ARCH_POWER))
+		if (Larch_power)
 			power *= arch_scale_smt_power(sd, cpu);
 		else
 			power *= default_scale_smt_power(sd, cpu);
@@ -3793,7 +3793,7 @@ static void update_cpu_power(struct sched_domain *sd, int cpu)
 
 	sdg->sgp->power_orig = power;
 
-	if (sched_feat(ARCH_POWER))
+	if (Larch_power)
 		power *= arch_scale_freq_power(sd, cpu);
 	else
 		power *= default_scale_freq_power(sd, cpu);
