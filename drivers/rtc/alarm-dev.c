c62e42ab3378dab9c78392c1aa57f934a0da1b25
Vijayakuamar Badiger <badiger@motorola.com>
2014-10-15 09:38:56 +0530

    - rtc: alarm: sanity check new time.
    Unexpected behavior could occur if invalid new time is used to
    update alarms. Add sanity checking for the new time.
    
    Change-Id: I9c7513a358fcedd52b302c4b30bbac0eba0fd951
    Signed-off-by: Vijayakuamar Badiger <badiger@motorola.com>
    Reviewed-on: http://gerrit.mot.com/679159
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Sudharsan Yettapu <sudharsan.yettapu@motorola.com>
    Reviewed-by: Maulik Desai <amd093@motorola.com>
    Submit-Approved: Maulik Desai <amd093@motorola.com>

--- drivers/rtc/alarm-dev.c
+++ drivers/rtc/alarm-dev.c
@@ -123,6 +123,12 @@ static long alarm_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 			rv = -EFAULT;
 			goto err1;
 		}
+		if (!timespec_valid_strict(&new_alarm_time)) {
+			pr_alarm(INFO, "Invalid new alarm: %ld.%09ld\n",
+				new_alarm_time.tv_sec, new_alarm_time.tv_nsec);
+			rv = -EINVAL;
+			goto err1;
+		}
 from_old_alarm_set:
 		spin_lock_irqsave(&alarm_slock, flags);
 		pr_alarm(IO, "alarm %d set %ld.%09ld\n", alarm_type,
@@ -163,7 +169,13 @@ from_old_alarm_set:
 			rv = -EFAULT;
 			goto err1;
 		}
-		rv = android_alarm_set_rtc(new_rtc_time);
+		if (!timespec_valid_strict(&new_rtc_time)) {
+			pr_alarm(INFO, "Invalid new rtc time: %ld.%09ld\n",
+				new_rtc_time.tv_sec, new_rtc_time.tv_nsec);
+			rv = -EINVAL;
+			goto err1;
+		}
+		rv = alarm_set_rtc(new_rtc_time);
 		spin_lock_irqsave(&alarm_slock, flags);
 		alarm_pending |= ANDROID_ALARM_TIME_CHANGE_MASK;
 		wake_up(&alarm_wait_queue);
