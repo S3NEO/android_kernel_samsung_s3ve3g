c311a3588bc91c7310f9003ebe851bda8d4adab9
kn06497 <kn06497@hotmail.com>
2017-04-16 18:30:42 -0500

    UPSTREAM: selinux: enable per-file labeling for debugfs files. UPSTREAM: selinux: enable per-file labeling for debugfs files.
    Add support for per-file labeling of debugfs files so that
    we can distinguish them in policy.  This is particularly
    important in Android where certain debugfs files have to be writable
    by apps and therefore the debugfs directory tree can be read and
    searched by all.
    
    Since debugfs is entirely kernel-generated, the directory tree is
    immutable by userspace, and the inodes are pinned in memory, we can
    simply use the same approach as with proc and label the inodes from
    policy based on pathname from the root of the debugfs filesystem.
    Generalize the existing labeling support used for proc and reuse it
    for debugfs too.
    
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>
    Signed-off-by: Paul Moore <pmoore@redhat.com>
    (cherry picked from commit 134509d54e4e98888be2697a92cb4b48957b792b)
    Signed-off-by: Stephen Barber <smbarber@chromium.org>
    
    BUG=none
    TEST=kernel compiles and boots
    
    Change-Id: Ib605028e3f43be25724a3e8ec60d40f54e52e7ac
    Reviewed-on: https://chromium-review.googlesource.com/281743
    Reviewed-by: Dylan Reid <dgreid@chromium.org>
    Tested-by: Stephen Barber <smbarber@chromium.org>
    Commit-Queue: Stephen Barber <smbarber@chromium.org>
    
    Signed-off-by: kn06497 <kn06497@hotmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -1346,7 +1346,8 @@ static int inode_doinit_with_dentry(struct inode *inode, struct dentry *opt_dent
 		/* Default to the fs superblock SID. */
 		isec->sid = sbsec->sid;
 
-		if ((sbsec->flags & SE_SBPROC) && !S_ISLNK(inode->i_mode)) {
+
+		if ((sbsec->flags & SE_SBGENFS) && !S_ISLNK(inode->i_mode)) {
 			/* We must have a dentry to determine the label on
 			 * procfs inodes */
 			if (opt_dentry)
@@ -1369,7 +1370,8 @@ static int inode_doinit_with_dentry(struct inode *inode, struct dentry *opt_dent
 			if (!dentry)
 				goto out_unlock;
 			isec->sclass = inode_mode_to_security_class(inode->i_mode);
-			rc = selinux_proc_get_sid(dentry, isec->sclass, &sid);
+			rc = selinux_genfs_get_sid(dentry, isec->sclass,
+						   sbsec->flags, &sid);
 			dput(dentry);
 			if (rc)
 				goto out_unlock;
