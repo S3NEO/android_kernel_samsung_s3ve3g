7620cd4e1b8a13e20e2047d1bb6b404fd89e5266
ppajda <pajdek@gmail.com>
2017-10-16 13:14:47 +0200

    Hotplugs: Add Blu, Alucard, Mako, Bricked, Intelli_plug, Msm, ASMP
    Change-Id: I372471d213d1f5f38ae38a2dea65c0baf5e57216

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -483,7 +483,26 @@ DECLARE_PER_CPU(struct rq, runqueues);
 #define task_rq(p)		cpu_rq(task_cpu(p))
 #define cpu_curr(cpu)		(cpu_rq(cpu)->curr)
 #define raw_rq()		(&__raw_get_cpu_var(runqueues))
+#if defined(CONFIG_INTELLI_PLUG) || defined(CONFIG_ALUCARD_HOTPLUG)
+struct nr_stats_s {
+	/* time-based average load */
+	u64 nr_last_stamp;
+	unsigned int ave_nr_running;
+	seqcount_t ave_seqcnt;
+};
 
+/* 27 ~= 134217728ns = 134.2ms
+ * 26 ~=  67108864ns =  67.1ms
+ * 25 ~=  33554432ns =  33.5ms
+ * 24 ~=  16777216ns =  16.8ms
+ */
+#define NR_AVE_PERIOD_EXP	27
+#define NR_AVE_SCALE(x)		((x) << FSHIFT)
+#define NR_AVE_PERIOD		(1 << NR_AVE_PERIOD_EXP)
+#define NR_AVE_DIV_PERIOD(x)	((x) >> NR_AVE_PERIOD_EXP)
+
+DECLARE_PER_CPU(struct nr_stats_s, runqueue_stats);
+#endif
 #ifdef CONFIG_SMP
 
 #define rcu_dereference_check_sched_domain(p) \
@@ -924,18 +943,62 @@ extern void cpuacct_charge(struct task_struct *tsk, u64 cputime);
 #else
 static inline void cpuacct_charge(struct task_struct *tsk, u64 cputime) {}
 #endif
-
-static inline void inc_nr_running(struct rq *rq)
+#if defined(CONFIG_INTELLI_PLUG) || defined(CONFIG_ALUCARD_HOTPLUG)
+static inline unsigned int do_avg_nr_running(struct rq *rq)
 {
-	sched_update_nr_prod(cpu_of(rq), rq->nr_running, true);
-	rq->nr_running++;
-}
 
-static inline void dec_nr_running(struct rq *rq)
-{
-	sched_update_nr_prod(cpu_of(rq), rq->nr_running, false);
-	rq->nr_running--;
+	struct nr_stats_s *nr_stats = &per_cpu(runqueue_stats, rq->cpu);
+	unsigned int ave_nr_running = nr_stats->ave_nr_running;
+	s64 nr, deltax;
+
+	deltax = rq->clock_task - nr_stats->nr_last_stamp;
+	nr = NR_AVE_SCALE(rq->nr_running);
+
+	if (deltax > NR_AVE_PERIOD)
+		ave_nr_running = nr;
+	else
+		ave_nr_running +=
+			NR_AVE_DIV_PERIOD(deltax * (nr - ave_nr_running));
+
+	return ave_nr_running;
 }
+#endif
+
+ static inline void inc_nr_running(struct rq *rq)
+ {
+#if defined(CONFIG_INTELLI_PLUG) || defined(CONFIG_ALUCARD_HOTPLUG)
+	struct nr_stats_s *nr_stats = &per_cpu(runqueue_stats, rq->cpu);
+#endif
+
+ 	sched_update_nr_prod(cpu_of(rq), rq->nr_running, true);
+#if defined(CONFIG_INTELLI_PLUG) || defined(CONFIG_ALUCARD_HOTPLUG)
+	write_seqcount_begin(&nr_stats->ave_seqcnt);
+	nr_stats->ave_nr_running = do_avg_nr_running(rq);
+	nr_stats->nr_last_stamp = rq->clock_task;
+#endif
+ 	rq->nr_running++;
+#if defined(CONFIG_INTELLI_PLUG) || defined(CONFIG_ALUCARD_HOTPLUG)
+	write_seqcount_end(&nr_stats->ave_seqcnt);
+#endif
+ }
+ 
+ static inline void dec_nr_running(struct rq *rq)
+ {
+#if defined(CONFIG_INTELLI_PLUG) || defined(CONFIG_ALUCARD_HOTPLUG)
+	struct nr_stats_s *nr_stats = &per_cpu(runqueue_stats, rq->cpu);
+#endif
+
+ 	sched_update_nr_prod(cpu_of(rq), rq->nr_running, false);
+#if defined(CONFIG_INTELLI_PLUG) || defined(CONFIG_ALUCARD_HOTPLUG)
+	write_seqcount_begin(&nr_stats->ave_seqcnt);
+	nr_stats->ave_nr_running = do_avg_nr_running(rq);
+	nr_stats->nr_last_stamp = rq->clock_task;
+#endif
+ 	rq->nr_running--;
+#if defined(CONFIG_INTELLI_PLUG) || defined (CONFIG_ALUCARD_HOTPLUG)
+	write_seqcount_end(&nr_stats->ave_seqcnt);
+#endif
+ }
 
 extern void update_rq_clock(struct rq *rq);
 
