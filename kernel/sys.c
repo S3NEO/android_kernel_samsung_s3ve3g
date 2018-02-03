92d1ca2ba5cc1861f6530c415776373397e652b8
Dicky Herlambang <herlambangdicky5@gmail.com>
2018-02-03 15:56:17 +0700

    Linux Kernel v3.4.41
    Applied patch from git.kernel.org with tag linux-3.4.41
    
    This is a stable release, adapted for flamingo
    
    Conflicts :
    	drivers/mtd/mtdchar.c:1123
    	include/linux/kref.h:93
    
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -374,7 +374,6 @@ void kernel_restart_prepare(char *cmd)
 	system_state = SYSTEM_RESTART;
 	usermodehelper_disable();
 	device_shutdown();
-	syscore_shutdown();
 }
 
 /**
@@ -423,6 +422,7 @@ void kernel_restart(char *cmd)
 #endif
 	kernel_restart_prepare(cmd);
 	disable_nonboot_cpus();
+	syscore_shutdown();
 	if (!cmd)
 		printk(KERN_EMERG "Restarting system.\n");
 	else
@@ -448,6 +448,7 @@ static void kernel_shutdown_prepare(enum system_states state)
 void kernel_halt(void)
 {
 	kernel_shutdown_prepare(SYSTEM_HALT);
+	disable_nonboot_cpus();
 	syscore_shutdown();
 	printk(KERN_EMERG "System halted.\n");
 	kmsg_dump(KMSG_DUMP_HALT);
