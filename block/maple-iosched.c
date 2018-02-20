37d36ddf0791a98b89f3964e2ae0848e78c5295a
infus38 <infus38@gmail.com>
2018-02-20 23:34:50 +0100

    maple io #2 thanks to @Nicklas373 based on that commit : https://github.com/Nicklas373/renaissance/commit/20dd53c64ca346efcbf47a0e3a22f202ed611c06

--- block/maple-iosched.c
+++ block/maple-iosched.c
@@ -5,6 +5,7 @@
  * Copyright (C) 2016 Joe Maples <joe@frap129.org>
  *           (C) 2012 Brandon Berhent <bbedward@gmail.com
  *           (C) 2012 Miguel Boton <mboton@gmail.com>
+ *	     (C) 2017 Sudokamikaze <pulshencode@outlook.com> Maple workaround for 3.4
  *
  * Maple uses a first come first serve style algorithm with seperated read/write
  * handling to allow for read biases. By prioritizing reads, simple tasks should improve
@@ -17,20 +18,18 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/slab.h>
-#ifdef CONFIG_POWERSUSPEND
-#include <linux/powersuspend.h>
-#endif
+#include <linux/display_state.h>
 
 #define MAPLE_IOSCHED_PATCHLEVEL	(8)
 
 enum { ASYNC, SYNC };
 
 /* Tunables */
-static const int sync_read_expire = 100;	/* max time before a read sync is submitted. */
-static const int sync_write_expire = 350;	/* max time before a write sync is submitted. */
-static const int async_read_expire = 200;	/* ditto for read async, these limits are SOFT! */
-static const int async_write_expire = 500;	/* ditto for write async, these limits are SOFT! */
-static const int fifo_batch = 16;		/* # of sequential requests treated as one by the above parameters. */
+static const int sync_read_expire = 50;	/* max time before a read sync is submitted. */
+static const int sync_write_expire = 200;	/* max time before a write sync is submitted. */
+static const int async_read_expire = 100;	/* ditto for read async, these limits are SOFT! */
+static const int async_write_expire = 250;	/* ditto for write async, these limits are SOFT! */
+static const int fifo_batch = 8;		/* # of sequential requests treated as one by the above parameters. */
 static const int writes_starved = 4;		/* max times reads can starve a write */
 static const int sleep_latency_multiple = 10;	/* multple for expire time when device is asleep */
 
@@ -80,28 +79,23 @@ maple_add_request(struct request_queue *q, struct request *rq)
 	struct maple_data *mdata = maple_get_data(q);
 	const int sync = rq_is_sync(rq);
 	const int dir = rq_data_dir(rq);
+	const bool display_on = is_display_on();
 
 	/*
 	 * Add request to the proper fifo list and set its
 	 * expire time.
 	 */
-#ifdef CONFIG_POWERSUSPEND
+
    	/* inrease expiration when device is asleep */
    	unsigned int fifo_expire_suspended = mdata->fifo_expire[sync][dir] * sleep_latency_multiple;
-   	if (!power_suspended && mdata->fifo_expire[sync][dir]) {
+   	if (display_on && mdata->fifo_expire[sync][dir]) {
    		rq_set_fifo_time(rq, jiffies + mdata->fifo_expire[sync][dir]);
    		list_add_tail(&rq->queuelist, &mdata->fifo_list[sync][dir]);
-   	} else if (!power_suspended && fifo_expire_suspended) {
+   	} else if (!display_on && fifo_expire_suspended) {
    		rq_set_fifo_time(rq, jiffies + fifo_expire_suspended);
    		list_add_tail(&rq->queuelist, &mdata->fifo_list[sync][dir]);
    	}
-#else
-    	if (mdata->fifo_expire[sync][dir]) {
-    		rq_set_fifo_time(rq, jiffies + mdata->fifo_expire[sync][dir]);
-    		list_add_tail(&rq->queuelist, &mdata->fifo_list[sync][dir]);
-    	}
 }
-#endif
 
 static struct request *
 maple_expired_request(struct maple_data *mdata, int sync, int data_dir)
@@ -163,12 +157,12 @@ maple_choose_expired_request(struct maple_data *mdata)
 static struct request *
 maple_choose_request(struct maple_data *mdata, int data_dir)
 {
+
 	struct list_head *sync = mdata->fifo_list[SYNC];
 	struct list_head *async = mdata->fifo_list[ASYNC];
 
 	/* Increase (non-expired-)batch-counter */
 	mdata->batched++;
-
 
 	/*
 	 * Retrieve request from available fifo list.
@@ -213,6 +207,7 @@ maple_dispatch_requests(struct request_queue *q, int force)
 	struct maple_data *mdata = maple_get_data(q);
 	struct request *rq = NULL;
 	int data_dir = READ;
+	const bool display_on = is_display_on();
 
 	/*
 	 * Retrieve any expired request after a batch of
@@ -223,16 +218,11 @@ maple_dispatch_requests(struct request_queue *q, int force)
 
 	/* Retrieve request */
 	if (!rq) {
-#ifdef CONFIG_POWERSUSPEND
 		/* Treat writes fairly while suspended, otherwise allow them to be starved */
-		if (!power_suspended && mdata->starved >= mdata->writes_starved)
- 			data_dir = WRITE;
- 		else if (power_suspended && mdata->starved >= 1)
+		if (display_on && mdata->starved >= mdata->writes_starved)
 			data_dir = WRITE;
-#else
- 		if (mdata->starved >= mdata->writes_starved)
+		else if (!display_on && mdata->starved >= 1)
 			data_dir = WRITE;
-#endif
 
 		rq = maple_choose_request(mdata, data_dir);
 		if (!rq)
