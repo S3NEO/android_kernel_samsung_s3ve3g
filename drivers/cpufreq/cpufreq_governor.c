3b1bd5f78bbd46ff8ef843e4580aed9962e06af6
infus38 <infus38@gmail.com>
2018-04-05 21:03:20 +0200

    driver&net&arc: INIT_DELAYED_WORK_DEFERRABLE should be INIT_DEFERRABLE_WORK
    Change-Id: I418dd4e755ea2b0fe505b8fcf50a7c4e55b70dd4

--- drivers/cpufreq/cpufreq_governor.c
+++ drivers/cpufreq/cpufreq_governor.c
@@ -383,7 +383,7 @@ int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 					kcpustat_cpu(j).cpustat[CPUTIME_NICE];
 
 			mutex_init(&j_cdbs->timer_mutex);
-			INIT_DELAYED_WORK_DEFERRABLE(&j_cdbs->work,
+			INIT_DEFERRABLE_WORK(&j_cdbs->work,
 					     dbs_data->cdata->gov_dbs_timer);
 		}
 
