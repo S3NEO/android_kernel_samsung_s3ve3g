59634b874422a32c9df5704d50c21089039e2e4f
Y-T-G <yaseenthegamer@gmail.com>
2018-05-10 12:01:25 -0700

    cpufreq: Add darkness governor
    cpufreq: Fix darkness governor errors
    
    cpufreq: Add darkness governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -447,10 +447,13 @@ extern struct cpufreq_governor cpufreq_gov_bioshock;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_zzmoove)
-#endif
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
+#endif
 
 
 /*********************************************************************
