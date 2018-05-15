bc9ab107f71d8073f14c074fa4035bd5c170a09d
leonardo meitz <leonardomeitz@gmail.com>
2018-05-15 17:04:43 +0200

    convert rtc alarm to android_alarm to get thermal and power driver running while still supporting alarmtimer :P

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -30,7 +30,7 @@
 #include <linux/of.h>
 #include <linux/sysfs.h>
 #include <linux/types.h>
-#include <linux/hrtimer.h>
+#include <linux/android_alarm.h>
 #include <linux/thermal.h>
 #include <mach/rpm-regulator.h>
 #include <mach/rpm-regulator-smd.h>
@@ -48,7 +48,7 @@ static bool core_control_enabled;
 static uint32_t cpus_offlined;
 static DEFINE_MUTEX(core_control_mutex);
 static uint32_t wakeup_ms;
-static struct hrtimer thermal_rtc_hrtimer;
+static struct alarm thermal_rtc;
 static struct kobject *tt_kobj;
 static struct kobject *cc_kobj;
 static struct work_struct timer_work;
@@ -1238,11 +1238,11 @@ static void thermal_rtc_setup(void)
 	ktime_t wakeup_time;
 	ktime_t curr_time;
 
-	curr_time = ktime_get_boottime();
+	curr_time = android_alarm_get_elapsed_realtime();
 	wakeup_time = ktime_add_us(curr_time,
 			(wakeup_ms * USEC_PER_MSEC));
-	hrtimer_start_range_ns(&thermal_rtc_hrtimer, wakeup_time,
-			ULONG_MAX, HRTIMER_MODE_ABS);
+	android_alarm_start_range(&thermal_rtc, wakeup_time,
+			wakeup_time);
 	pr_debug("%s: Current Time: %ld %ld, Alarm set to: %ld %ld\n",
 			KBUILD_MODNAME,
 			ktime_to_timeval(curr_time).tv_sec,
@@ -1257,15 +1257,13 @@ static void timer_work_fn(struct work_struct *work)
 	sysfs_notify(tt_kobj, NULL, "wakeup_ms");
 }
 
-enum hrtimer_restart thermal_rtc_callback(struct hrtimer *timer)
+static void thermal_rtc_callback(struct alarm *al)
 {
-	struct timespec ts;
-	get_monotonic_boottime(&ts);
+	struct timeval ts;
+	ts = ktime_to_timeval(android_alarm_get_elapsed_realtime());
 	schedule_work(&timer_work);
 	pr_debug("%s: Time on alarm expiry: %ld %ld\n", KBUILD_MODNAME,
-			ts.tv_sec, ts.tv_nsec / 1000);
-
-	return HRTIMER_NORESTART;
+			ts.tv_sec, ts.tv_usec);
 }
 
 static int hotplug_notify(enum thermal_trip_type type, int temp, void *data)
@@ -1719,7 +1717,7 @@ static ssize_t store_wakeup_ms(struct kobject *kobj,
 		pr_debug("%s: Timer started for %ums\n", KBUILD_MODNAME,
 				wakeup_ms);
 	} else {
-		ret = hrtimer_cancel(&thermal_rtc_hrtimer);
+		ret = android_alarm_cancel(&thermal_rtc);
 		if (ret)
 			pr_debug("%s: Timer canceled\n", KBUILD_MODNAME);
 		else
@@ -2685,11 +2683,8 @@ int __init msm_thermal_late_init(void)
 	msm_thermal_add_psm_nodes();
 	msm_thermal_add_vdd_rstr_nodes();
 	msm_thermal_add_ocr_nodes();
-	hrtimer_init(&thermal_rtc_hrtimer,
-			CLOCK_BOOTTIME,
-			HRTIMER_MODE_ABS);
-	thermal_rtc_hrtimer.function=
-			&thermal_rtc_callback;
+	android_alarm_init(&thermal_rtc, ANDROID_ALARM_ELAPSED_REALTIME_WAKEUP,
+			thermal_rtc_callback);
 	INIT_WORK(&timer_work, timer_work_fn);
 	msm_thermal_add_timer_nodes();
 
