a2ed760fd68257a8dccf1a3e9139fa40f4875055
HASH <hash.xda@gmail.com>
2018-05-16 16:55:57 +0530

    Move suspend hooks to mdss_dsi.c Now the hooks are in mdss_dsi_panel.c Since we don't use mdss_dsi_panel.c and use these hooks in powesuspend and display state We need them enabled, so move it to mdss_dsi.c
    Change-Id: Id57d066de4151727da7dcae5e02dbbe94405f815
    Signed-off-by: HASH <hash.xda@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -22,6 +22,11 @@
 #include <linux/err.h>
 #include <linux/regulator/consumer.h>
 #include <linux/clk.h>
+#include <linux/display_state.h>
+
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
 
 #ifdef CONFIG_STATE_NOTIFIER
 #include <linux/state_notifier.h>
@@ -32,6 +37,13 @@
 #include "mdss_panel.h"
 #include "mdss_dsi.h"
 #include "mdss_debug.h"
+
+bool display_on = true;
+
+bool is_display_on()
+{
+	return display_on;
+}
 
 static unsigned char *mdss_dsi_base;
 int contsplash_lkstat = 0;
@@ -1138,6 +1150,10 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		ctrl_pdata->mdp_tg_on = 1;
 		if (ctrl_pdata->dsi_on_state == DSI_HS_MODE)
 			rc = mdss_dsi_unblank(pdata);
+		display_on = true;
+#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+#endif
 #ifdef CONFIG_STATE_NOTIFIER
 		state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
 #endif
@@ -1152,10 +1168,14 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->dsi_off_state == DSI_LP_MODE)
 			rc = mdss_dsi_blank(pdata);
 		rc = mdss_dsi_off(pdata);
-		break;
+		display_on = false;
+#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+#endif
 #ifdef CONFIG_STATE_NOTIFIER
 		state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
 #endif
+		break;
 #if defined(CONFIG_FB_MSM_MDSS_S6E8AA0A_HD_PANEL)
 	case MTP_READ:
 		rc = mdss_MTP_read(pdata);
