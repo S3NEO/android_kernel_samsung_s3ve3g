a29f3ae0d796521172320a05e4d63b605ec6acc6
h2o64 <theh2o64@gmail.com>
2018-05-18 15:30:27 -0700

    ARM : Add underclock/overclock support
    defconfig: Enable CPU overclock and underclock

--- arch/arm/mach-msm/acpuclock-8226.c
+++ arch/arm/mach-msm/acpuclock-8226.c
@@ -60,10 +60,15 @@ static struct msm_bus_scale_pdata bus_client_pdata = {
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p1[] = {
-	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
-	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
-	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
-	{ 1,  787200, ACPUPLL, 5, 0,   CPR_CORNER_4, 0, 6 },
+#ifdef	CONFIG_CPU_UNDERCLOCK
+	{ 1,   96000, ACPUPLL, 5, 2,   CPR_CORNER_2,   0, 4 },
+	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,   0, 4 },
+	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,   0, 4 },
+#endif
+	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,   0, 4 },
+	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,   0, 4 },
+	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4,   0, 6 },
+	{ 1,  787200, ACPUPLL, 5, 0,   CPR_CORNER_4,   0, 6 },
 	{ 1,  998400, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 7 },
 	{ 1, 1094400, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 7 },
 	{ 0, 1190400, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 7 },
@@ -71,6 +76,11 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p1[] = {
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p2[] = {
+#ifdef	CONFIG_CPU_UNDERCLOCK
+	{ 1,   96000, ACPUPLL, 5, 2,   CPR_CORNER_2,   0, 4 },
+	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,   0, 4 },
+	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,   0, 4 },
+#endif
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
@@ -82,6 +92,11 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p2[] = {
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p4[] = {
+#ifdef	CONFIG_CPU_UNDERCLOCK
+	{ 1,   96000, ACPUPLL, 5, 2,   CPR_CORNER_2,   0, 4 },
+	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,   0, 4 },
+	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,   0, 4 },
+#endif
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
@@ -96,6 +111,11 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p4[] = {
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p6[] = {
+#ifdef	CONFIG_CPU_UNDERCLOCK
+	{ 1,   96000, ACPUPLL, 5, 2,   CPR_CORNER_2,   0, 4 },
+	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,   0, 4 },
+	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,   0, 4 },
+#endif
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
@@ -148,8 +168,13 @@ static struct acpuclk_drv_data drv_data = {
 		.update_mask = RCG_CONFIG_UPDATE_BIT,
 		.poll_mask = RCG_CONFIG_UPDATE_BIT,
 	},
+#ifdef CONFIG_CPU_UNDERCLOCK
+	.power_collapse_khz = 96000,
+	.wait_for_irq_khz = 96000,
+#else
 	.power_collapse_khz = 300000,
 	.wait_for_irq_khz = 300000,
+#endif
 };
 
 static int __init acpuclk_a7_probe(struct platform_device *pdev)
--- arch/arm/mach-msm/clock-a7.c
+++ arch/arm/mach-msm/clock-a7.c
@@ -245,6 +245,11 @@ static void get_speed_bin(struct platform_device *pdev, int *bin, int *version)
 	valid = (pte_efuse >> 3) & 0x1;
 	*version = (pte_efuse >> 4) & 0x3;
 
+#ifdef CONFIG_CPU_OVERCLOCK
+	dev_info(&pdev->dev, "Speed bin being set to 1 for overclock!\n");
+	*bin = 1;
+#else
+
 	if (redundant_sel == 1)
 		*bin = (pte_efuse >> 27) & 0x7;
 
@@ -254,6 +259,7 @@ static void get_speed_bin(struct platform_device *pdev, int *bin, int *version)
 	} else {
 		dev_info(&pdev->dev, "Speed bin: %d\n", *bin);
 	}
+#endif
 
 	dev_info(&pdev->dev, "PVS version: %d\n", *version);
 
