4b53fac037d9c04c2bbca87f39499a8a6556f051
HASH <hash.xda@gmail.com>
2018-05-22 12:20:30 +0530

    Revert "audit: silence spammy debug logs"
    This reverts commit acc806703dd99dbb36215554bb589abdca85c9c9.

--- kernel/audit.c
+++ kernel/audit.c
@@ -391,10 +391,9 @@ static void audit_printk_skb(struct sk_buff *skb)
 #endif
 
 	if (nlh->nlmsg_type != AUDIT_EOE) {
-		if (printk_ratelimit())
-			pr_debug(KERN_NOTICE "type=%d %s\n", nlh->nlmsg_type, data);
-		else
-			audit_log_lost("printk limit exceeded\n");
+#ifdef CONFIG_PROC_AVC
+		sec_avc_log("%s\n", data);
+#endif
 	}
 
 	audit_hold_skb(skb);
@@ -732,7 +731,7 @@ static int audit_receive_msg(struct sk_buff *skb, struct nlmsghdr *nlh)
 							audit_pid, loginuid,
 							sessionid, sid, 1);
 
-			audit_pid = 0;
+			audit_pid = new_pid;
 			audit_nlk_pid = NETLINK_CB(skb).pid;
 		}
 		if (status_get->mask & AUDIT_STATUS_RATE_LIMIT) {
