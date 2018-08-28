e0231ca5fa04b6c0d580d67f7c61dbcb3654b40d
PythonLimited <leonardomeitz@gmail.com>
2018-08-28 14:56:43 +0200

    fix duplicates from cherry-pick

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1368,8 +1368,6 @@ struct task_struct {
 #endif
 	unsigned long atomic_flags; /* Flags needing atomic access. */
 
-	unsigned long atomic_flags; /* Flags needing atomic access. */
-
 	pid_t pid;
 	pid_t tgid;
 
