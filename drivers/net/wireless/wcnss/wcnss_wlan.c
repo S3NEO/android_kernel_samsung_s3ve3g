55b83e86a1ec14f9c038bc3e02b4ced10470285c
xXPR0T0TYPEXx <thekurdo97@gmail.com>
2019-02-21 23:37:46 +0100

    wcnss: wcnss_log_iris_regs expecting CONFIG_WCNSS_IRIS_REGISTER_DUMP now

--- drivers/net/wireless/wcnss/wcnss_wlan.c
+++ drivers/net/wireless/wcnss/wcnss_wlan.c
@@ -875,7 +875,7 @@ void wcnss_pronto_log_debug_regs(void)
 }
 EXPORT_SYMBOL(wcnss_pronto_log_debug_regs);
 
-#ifdef CONFIG_WCNSS_REGISTER_DUMP_ON_BITE
+#ifdef CONFIG_WCNSS_IRIS_REGISTER_DUMP
 static void wcnss_log_iris_regs(void)
 {
 	int i;
@@ -891,7 +891,9 @@ static void wcnss_log_iris_regs(void)
 		pr_info("[0x%08x] : 0x%08x\n", regs_array[i], reg_val);
 	}
 }
+#endif
 
+#ifdef CONFIG_WCNSS_REGISTER_DUMP_ON_BITE
 int wcnss_get_mux_control(void)
 {
 	void __iomem *pmu_conf_reg;
@@ -929,11 +931,16 @@ void wcnss_log_debug_regs_on_bite(void)
 
 		if (clk_rate) {
 			wcnss_pronto_log_debug_regs();
-			if (wcnss_get_mux_control())
+			if (wcnss_get_mux_control()){
+#ifdef CONFIG_WCNSS_IRIS_REGISTER_DUMP
 				wcnss_log_iris_regs();
+#endif
+			}
 		} else {
 			pr_err("clock frequency is zero, cannot access PMU or other registers\n");
+#ifdef CONFIG_WCNSS_IRIS_REGISTER_DUMP
 			wcnss_log_iris_regs();
+#endif
 		}
 	}
 }
@@ -944,8 +951,11 @@ void wcnss_reset_intr(void)
 {
 	if (wcnss_hardware_type() == WCNSS_PRONTO_HW) {
 		wcnss_pronto_log_debug_regs();
-		if (wcnss_get_mux_control())
+		if (wcnss_get_mux_control()){
+#ifdef CONFIG_WCNSS_IRIS_REGISTER_DUMP
 			wcnss_log_iris_regs();
+#endif
+		}
 		wmb();
 		__raw_writel(1 << 16, penv->fiq_reg);
 	} else {
@@ -963,8 +973,11 @@ void wcnss_reset_fiq(bool clk_chk_en)
 			wcnss_log_debug_regs_on_bite();
 		} else {
 			wcnss_pronto_log_debug_regs();
-			if (wcnss_get_mux_control())
+			if (wcnss_get_mux_control()){
+#ifdef CONFIG_WCNSS_IRIS_REGISTER_DUMP
 				wcnss_log_iris_regs();
+#endif
+			}
 		}
 		/* Insert memory barrier before writing fiq register */
 		wmb();
