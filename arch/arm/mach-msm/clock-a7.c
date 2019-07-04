0509b35e3e7b516978b1a4f57f3db9155bc049c8
xXPR0T0TYPEXx <thekurdo97@gmail.com>
2019-07-04 22:02:20 +0200

    msm: msm8228: possibility to force acpu_freq_tbl_8226_1p6
    Force using acpu_freq_tbl_8226_1p6 to unlock extra CPU Frequencies for devices with MSM8228 CPU by setting speed bin to 1. Config disabled by default.

--- arch/arm/mach-msm/clock-a7.c
+++ arch/arm/mach-msm/clock-a7.c
@@ -252,6 +252,9 @@ static void get_speed_bin(struct platform_device *pdev, int *bin, int *version)
 		dev_info(&pdev->dev, "Speed bin not set. Defaulting to 0!\n");
 		*bin = 0;
 	} else {
+#ifdef CONFIG_MSM8228_USE_ACPU_FREQ_TBL_1P6
+        *bin = 1;
+#endif
 		dev_info(&pdev->dev, "Speed bin: %d\n", *bin);
 	}
 
